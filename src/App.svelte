22ab2c92ce2b7e7a57435d95596739885e3a25df
Jintao Koong <jintaokoong99@gmail.com>
2021-09-02 12:41:30 +0800

    feat: add status handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Card, CardBody, Col, Container, Icon, Navbar, NavbarBrand, Row, Styles } from 'sveltestrap'
+  import { Alert, Card, CardBody, Col, Container, Icon, Navbar, NavbarBrand, Row, Spinner, Styles } from 'sveltestrap'
   import { SvelteToast, toast } from '@zerodevx/svelte-toast'
   import { onMount } from 'svelte'
   import CommandService from './services/command-service'
@@ -87,35 +87,53 @@
   }
 </script>
 
-<main>
-  <Container>
-    <Navbar color="light" light>
-      <NavbarBrand class="me-auto">指令查看器</NavbarBrand>
-    </Navbar>
-    <Row class="mt-4" cols={{ lg: 4, md: 2, sm: 1, xs: 1 }}>
-      {#each state.data as command}
-        <Col class="mb-3">
-          <Card>
-            <CardBody>
-              <div class="d-flex clickable rounded mb-1 justify-content-center align-items-center bg-dark" style="height: 15vh; min-height: 147px" on:click={onPlay(command._id, command.audio.src)}>
-                {#if playingids.includes(command._id)}
-                  <Icon name='pause-circle' class="fs-2" style="color: white"/>
-                {:else}
-                  <Icon name='play-circle' class="fs-2" style="color: white"/>
-                {/if}
-              </div>
-              <p class="title mb-1">{command.name}</p>
-              <p class="description mb-1">{command.description}</p>
-              <div class="clickable code-container rounded p-1" on:click={copy(`!s ${command.identifier}`)}>
-                <pre>!s {command.identifier}</pre>
-              </div>
-            </CardBody>
-          </Card>
-        </Col>
-      {/each}
-    </Row>
-  </Container>
-</main>
+<Container class="d-flex flex-column h-100">
+  <main class="flex-shrink-0">
+      <Navbar color="light" light>
+        <NavbarBrand class="me-auto">指令查看器</NavbarBrand>
+      </Navbar>
+      <div class="mt-4">
+        {#if state.isLoading && !state.isError}
+          <div class="d-flex justify-content-center align-items-center">
+            <Spinner size="sm" type="grow" /> <p class="ms-2 loading-message">載入中 請稍後</p>
+          </div>
+        {:else if state.isError}
+          <Alert color="danger">
+            <h5 class="alert-heading">歐不 看起來像是出現了錯誤</h5>
+            HTTP要求有可能被StreamElements 請通知亨利 謝謝
+          </Alert>
+        {:else}
+          <Row cols={{ lg: 4, md: 2, sm: 1, xs: 1 }}>
+            {#each state.data as command}
+              <Col class="mb-3">
+                <Card>
+                  <CardBody>
+                    <div class="d-flex clickable rounded mb-1 justify-content-center align-items-center bg-dark" style="height: 15vh; min-height: 147px" on:click={onPlay(command._id, command.audio.src)}>
+                      {#if playingids.includes(command._id)}
+                        <Icon name='pause-circle' class="fs-2" style="color: white"/>
+                      {:else}
+                        <Icon name='play-circle' class="fs-2" style="color: white"/>
+                      {/if}
+                    </div>
+                    <p class="title mb-1">{command.name}</p>
+                    <p class="description mb-1">{command.description}</p>
+                    <div class="clickable code-container rounded p-1" on:click={copy(`!s ${command.identifier}`)}>
+                      <pre>!s {command.identifier}</pre>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            {/each}
+          </Row>
+        {/if}
+      </div>
+  </main>
+  <footer class="mt-auto p-2 bg-light  text-center">
+    <span class="version">
+      v1.0.1
+    </span>
+  </footer>
+</Container>
 <SvelteToast
         options={{
     duration: 1000,
@@ -143,12 +161,20 @@
     margin: 0;
   }
 
+  .version {
+    font-size: 0.9rem;
+  }
+
   .clickable {
     cursor: pointer;
   }
 
   .code-container {
     background: beige;
+  }
+
+  .loading-message {
+    font-size: 1.25rem;
   }
 
   .title {
