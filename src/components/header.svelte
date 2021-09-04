103da434218f2e9c5f01b6b3d1460e266c69e60a
Jintao Koong <jintaokoong99@gmail.com>
2021-09-04 14:14:49 +0800

    feat: add link to twitch channel

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 import { _ } from 'svelte-i18n';
 
-  import { Navbar, NavbarBrand } from 'sveltestrap'
+  import { Nav, NavItem, NavLink, Icon, Navbar, NavbarBrand } from 'sveltestrap'
 </script>
 
 <div>
   <Navbar color="light" light>
     <NavbarBrand class="me-auto">{$_('brand')}</NavbarBrand>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink title="DD的頻道" href="https://www.twitch.tv/mtow9140" target="_blank"><Icon name="twitch" class="fs-3" style="color: #A970FF"/></NavLink>
+      </NavItem>
+    </Nav>
   </Navbar>
 </div>
-
-
